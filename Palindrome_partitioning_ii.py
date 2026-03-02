db5f8d44fa551096f12f9270091f7c617516a155
Kalpana J <j.kalpana.jks@gmail.com>
2026-03-02 22:44:23 +0530

    Add min_cut function for palindrome partitioning
    Implement min_cut function to calculate minimum palindrome cuts.

---
+++ Palindrome_partitioning_ii.py
@@ -0,0 +1,24 @@
+def min_cut(s):
+    n = len(s)
+    
+    # Step 1: Precompute palindromes
+    is_pal = [[False] * n for _ in range(n)]
+    
+    for i in range(n - 1, -1, -1):
+        for j in range(i, n):
+            if s[i] == s[j] and (j - i < 2 or is_pal[i + 1][j - 1]):
+                is_pal[i][j] = True
+    
+    # Step 2: DP for minimum cuts
+    dp = [0] * n
+    
+    for i in range(n):
+        if is_pal[0][i]:
+            dp[i] = 0
+        else:
+            dp[i] = float('inf')
+            for j in range(i):
+                if is_pal[j + 1][i]:
+                    dp[i] = min(dp[i], dp[j] + 1)
+    
+    return dp[n - 1]
