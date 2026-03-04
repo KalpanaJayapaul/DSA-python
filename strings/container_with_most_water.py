b858e70016ed7ca77af7be783ad6a76b16e2bd00
Kalpana J <j.kalpana.jks@gmail.com>
2026-03-04 22:43:48 +0530

    Implement max_area function to calculate water area

---
+++ strings/container_with_most_water.py
@@ -0,0 +1,17 @@
+def max_area(height):
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+    
+    while left < right:
+        width = right - left
+        current_height = min(height[left], height[right])
+        max_water = max(max_water, width * current_height)
+        
+        # Move the smaller pointer
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    
+    return max_water
